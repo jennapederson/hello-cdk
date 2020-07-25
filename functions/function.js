0a6aa25e0bc5093dc2bb52589e9a2c2f449d359b
Jenna Pederson <jennapederson@gmail.com>
2020-07-24 21:53:32 -0500

    Put item into dynamodb when saying hello

--- functions/function.js
+++ functions/function.js
@@ -1,19 +1,47 @@
+const AWS = require('aws-sdk');
+
+const ddb = new AWS.DynamoDB.DocumentClient();
+
 exports.handler = async function (event) {
   console.log("request: ", JSON.stringify(event));
-  
-  // spit out the environment var we passed in
-  console.log('HELLO_TABLE_NAME: ', process.env.HELLO_TABLE_NAME);
+  let name = getName(event);
 
-  return sendRes(200, "Hello Jenna!");
+  try {
+    // log our hello to dynamodb
+    const data = await ddb.put({
+      TableName: process.env.HELLO_TABLE_NAME,
+      Item: {
+          name: name,
+          message: `Said hello to ${name}`,
+      },
+    }).promise();
+
+    return sendHtmlResponse(200, `Hello ${name}!`)
+  } catch(error) {
+    console.error(error);
+    return sendHtmlResponse(500, 'There was an unknown error.')
+  }
 }
 
-const sendRes = (status, body) => {
-  var response = {
-    statusCode: status,
+const getName = (event) => {
+  let name = undefined;
+  if (event["queryStringParameters"] && ("name" in event["queryStringParameters"])) {
+    name = event["queryStringParameters"]["name"];
+  }
+
+  if (!name) {
+    name = "World";
+  }
+  return name;
+}
+
+const sendHtmlResponse = (status, body) => {
+  return {
+    statusCode: status, 
+    body: body,
     headers: {
       "Content-Type": "text/html",      
-    },
-    body: body,
+    },     
   };
-  return response;
-};
+}
+
